4681c513bfc7877872044f1b0151ea5636a681ea
kevlened <boyettel@gmail.com>
2018-05-06 21:00:49 -0700

    Clean before building

--- tasks.py
+++ tasks.py
@@ -1,10 +1,17 @@
 from invoke import task
 from codecs import open
 from os import path
+import shutil
 import re
 
 with open(path.join('pytest_parallel', '__init__.py'), encoding='utf-8') as f:
     version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
+
+
+def clean():
+    shutil.rmtree('build')
+    shutil.rmtree('dist')
+    shutil.rmtree('pytest_parallel.egg-info')
 
 
 @task
@@ -14,6 +21,7 @@ def test(c):
 
 @task
 def build(c):
+    clean()
     c.run('python setup.py sdist bdist_wheel')
 
 
