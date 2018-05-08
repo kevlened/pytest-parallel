aa72ba833fa912a8d0e4750d1c80404f06704319
kevlened <boyettel@gmail.com>
2018-05-08 09:32:02 -0700

    Parse install_requires from Pipfile

--- setup.py
+++ setup.py
@@ -3,15 +3,21 @@ from codecs import open
 from os import path
 import re
 
-here = path.abspath(path.dirname(__file__))
-
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join('README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
 # Get the version from the module
 with open(path.join('pytest_parallel', '__init__.py'), encoding='utf-8') as f:
     version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
+
+# Get the install_requires from the Pipfile
+with open(path.join('Pipfile'), encoding='utf-8') as f:
+    pkgs = re.search(r'^\[packages\]([^\[]*)', f.read(), re.M).group(1).strip()
+    pkg_pattern = re.compile(r'"?(.*?)"? = "(.*?)"')
+    install_requires = [name if version == '*' else name + version
+                        for (name, version)
+                        in re.findall(pkg_pattern, pkgs)]
 
 setup(
     name='pytest-parallel',
@@ -24,7 +30,8 @@ setup(
     author='Browsertron',
     author_email='team@browsertron.com',
 
-    install_requires=['pytest>=3.0.0'],
+    include_package_data=True,
+    install_requires=install_requires,
     packages=['pytest_parallel'],
     entry_points={
         'pytest11': [
