b38c5d61dc2db34bec396cab9a22de43826cb16b
kevlened <boyettel@gmail.com>
2018-05-08 09:56:48 -0700

    Refactor clean into build function

--- tasks.py
+++ tasks.py
@@ -8,12 +8,6 @@ with open(path.join('pytest_parallel', '__init__.py'), encoding='utf-8') as f:
     version = re.search(r'__version__ = \'(.*?)\'', f.read()).group(1)
 
 
-def clean():
-    shutil.rmtree('build')
-    shutil.rmtree('dist')
-    shutil.rmtree('pytest_parallel.egg-info')
-
-
 @task
 def test(c):
     c.run('tox')
@@ -21,7 +15,9 @@ def test(c):
 
 @task
 def build(c):
-    clean()
+    shutil.rmtree('build')
+    shutil.rmtree('dist')
+    shutil.rmtree('pytest_parallel.egg-info')
     c.run('python setup.py sdist bdist_wheel')
 
 
