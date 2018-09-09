8a0203f6948201ea65850716d9439340dc35fb7d
kevlened <boyettel@gmail.com>
2018-09-09 10:19:40 -0700

    Ensure original os.environ methods exist

---
+++ CONTRIBUTING.md
@@ -0,0 +1,27 @@
+# How to contribute
+
+Thanks for asking! We graciously accept PRs, assuming they meet quality standards and align with the direction of the project. If they don't, we'll do our best to help you understand what to modify in order to merge.
+
+## Setting up your environment
+
+1) Install pipenv and Python 3.7 if not already installed
+2) `git clone git@github.com:browsertron/pytest-parallel.git`
+3) `cd pytest-parallel`
+4) `pipenv install --dev`
+5) `pipenv run test` - if this fails due to missing Python versions, see Testing below.
+
+Congrats! You should have a working dev environment now.
+
+## Testing
+
+No PR will be merged without tests or test modifications unless the PR is a non-functional change (docs, text, etc). We test across Python versions using tox (listed in `tox.ini`). If you're missing a Python version, we recommend `pyenv` to install the versions you need.
+
+Assuming you've set up your environment, you can run `pipenv run test` to run the tests.
+
+## Installing pyenv on OSX
+
+1) `brew install pyenv`
+2) `pyenv init` - this prompts you to add to `~/.bash_profile`
+3) `pyenv install x.x.x`
+4) `pyenv local 3.6.x 3.7.x 3.x.x ...` to set the patch version for each minor version
+5) Ensure that running `python3.6` starts a REPL and doesn't list available versions
--- tests/test_general.py
+++ tests/test_general.py
@@ -1,3 +1,6 @@
+import pytest
+
+
 def test_help(testdir):
     result = testdir.runpytest(
         '--help',
@@ -58,3 +61,35 @@ def test_sanity_async_double(testdir):
     """)
     result = testdir.runpytest()
     result.assert_outcomes(passed=2)
+
+
+@pytest.mark.parametrize('cli_args', [
+  [],
+  ['--workers=2'],
+  ['--tests-per-worker=2']
+])
+def test_environ_shim(testdir, cli_args):
+    testdir.makepyfile("""
+        import os
+
+        def test_0():
+            expected = set([
+                '_MutableMapping__marker', '__abstractmethods__', '__class__',
+                '__contains__', '__delattr__', '__delitem__', '__dict__',
+                '__dir__', '__doc__', '__eq__', '__format__', '__ge__',
+                '__getattribute__', '__getitem__', '__gt__', '__hash__',
+                '__init__', '__init_subclass__', '__iter__', '__le__',
+                '__len__', '__lt__', '__module__', '__ne__', '__new__',
+                '__reduce__', '__reduce_ex__', '__repr__', '__reversed__',
+                '__setattr__', '__setitem__', '__sizeof__', '__slots__',
+                '__str__', '__subclasshook__', '__weakref__', '_data',
+                'clear', 'copy', 'decodekey', 'decodevalue', 'encodekey',
+                'encodevalue', 'get', 'items', 'keys', 'pop', 'popitem',
+                'putenv', 'setdefault', 'unsetenv', 'update', 'values'
+            ])
+            environ_keys = dir(os.environ)
+            assert list(set(expected) - set(environ_keys)) == []
+            assert list(os.environ.keys()) == list(os.environ.copy().keys())
+    """)
+    result = testdir.runpytest(*cli_args)
+    result.assert_outcomes(passed=1)
